2db005b476a8952559994c352b31ad32c198ebca
Phem Trevor <agengfirman3@gmail.com>
2020-10-07 17:41:46 +0700

    edit

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,13 +5,15 @@ module.exports = {
         res.render('admin/dashboard/view_dashboard');
     },
 
-    viewCategory: (req, res) => {
-        res.render('admin/category/view_category');
+    viewCategory: async(req, res) => {
+        const category = await Category.find();
+        // console.log(category);
+        res.render('admin/category/view_category', { category });
     },
 
     addCategory: async (req, res) => {
         const {name} = req.body;
-        //console.log(name);
+        // console.log(name);
         await Category.create({name});
         res.redirect('/admin/category');
     },
